261ebe46972252d10daeb0b1ba77c9b751b32d40
Michel Balamou <michelbalamou@gmail.com>
2019-01-22 21:35:26 -0500

    Change main view color to gradient

--- Indulge/Controller/ViewController.swift
+++ Indulge/Controller/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.layer.insertSublayer(Colors().background, at: 0)
     }
 
 
