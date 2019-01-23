40c29413c5a809464d9d740fae9a99f095aa3c3b
Michel Balamou <michelbalamou@gmail.com>
2019-01-22 21:08:04 -0500

    Initial Commit

---
+++ Indulge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Indulge
+//
+//  Created by Michel Balamou on 2019-01-22.
+//  Copyright © 2019 Michel Balamou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
